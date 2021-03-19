5458fb2b4f922aac936f99d829c9576c5f261646
grzegorzpnk <grzegorzpnk@gmail.com>
2021-03-19 02:13:41 +0100

    first version calendar

--- app/src/main/java/com/example/chalturnikcalendar/MainActivity.java
+++ app/src/main/java/com/example/chalturnikcalendar/MainActivity.java
@@ -1,16 +1,60 @@
 package com.example.chalturnikcalendar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.applandeo.materialcalendarview.EventDay;
+import com.kizitonwose.calendarview.ui.DayBinder;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends Activity implements android.widget.CalendarView.OnDateChangeListener{
+
+
+
+     CalendarView calendar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //first changes
+        String languageToLoad  = "pl"; // your language
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+        this.setContentView(R.layout.activity_main);
+
+        calendar = (CalendarView) findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(this);
+
+    }
+
+    @Override
+    public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int monthOfYear, int dayOfMonth) {
+        Calendar then = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+
+        Toast.makeText(this, then.getTime().toString(), Toast.LENGTH_LONG)
+                .show();
     }
 }
+
